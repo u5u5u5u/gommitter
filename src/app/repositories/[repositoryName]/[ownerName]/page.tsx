2ee3bf8f5798ccba1c8fe6f0b68e4e5a112ee71b
U5 <kyutech.joko@gmail.com>
2025-03-19 23:43:56 +0900

    add:コミットメッセージ投稿

--- src/app/repositories/[repositoryName]/[ownerName]/page.tsx
+++ src/app/repositories/[repositoryName]/[ownerName]/page.tsx
@@ -2,6 +2,7 @@
 
 import { use, useEffect, useState } from "react";
 import type { Commit } from "@/types/commit";
+import { createClient } from "@/utils/supabase/client";
 
 const CommitList = ({
   params,
@@ -28,12 +29,27 @@ const CommitList = ({
     fetchCommits();
   }, [repositoryName, ownerName]);
 
+  const post = async (message: string) => {
+    const supabase = createClient();
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError) {
+      console.error("Failed to get user", userError);
+    }
+    await supabase.from("commits").insert({
+      user_id: userData.user?.id,
+      message: message,
+    });
+  };
+
   return (
     <div>
       <h1>コミットメーセージ一覧</h1>
       <ul>
         {commits.map((commit) => (
-          <li key={commit.id}>{commit.message}</li>
+          <li key={commit.id}>
+            <p>{commit.message}</p>
+            <button onClick={() => post(commit.message)}>投稿</button>
+          </li>
         ))}
       </ul>
     </div>

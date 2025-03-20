0590c425dd9cc2843e36da8f0db7ff165193ffce
U5 <kyutech.joko@gmail.com>
2025-03-20 13:45:34 +0900

    update:ログイン画面ui修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,14 @@
 import SocialLoginButton from "@/components/auth/SocialLoginButton";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <SocialLoginButton provider="github" />
+      <Button className="">
+        <FaGithub />
+        <SocialLoginButton provider="github" />
+      </Button>
     </div>
   );
 };

54df4d4b094494e1e5f1b62ce59b66b852dd93b1
U5 <kyutech.joko@gmail.com>
2025-03-22 06:31:17 +0900

    privateリポジトリのコミットメッセージにアクセスできないのかーー

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -76,18 +76,22 @@ const getCommittedRepositories = async (accessToken: string) => {
         created_at: string;
       }
     >();
-    events.forEach((event: { repo: { id: number; name: string }; created_at: string }) => {
-      // if (event.type === "PushEvent") {
-      committedRepositories.set(event.repo.id, {
-        id: event.repo.id,
-        owner: event.repo.name.split("/")[0],
-        name: event.repo.name.split("/")[1],
-        created_at: event.created_at,
-      });
-      // }
-    });
-
-    // console.log("Committed repositories:", committedRepositories);
+    events.forEach(
+      (event: {
+        repo: { id: number; name: string };
+        created_at: string;
+        public: boolean;
+      }) => {
+        if (event.public) {
+          committedRepositories.set(event.repo.id, {
+            id: event.repo.id,
+            owner: event.repo.name.split("/")[0],
+            name: event.repo.name.split("/")[1],
+            created_at: event.created_at,
+          });
+        }
+      }
+    );
 
     return Array.from(committedRepositories.values());
   } catch (error) {

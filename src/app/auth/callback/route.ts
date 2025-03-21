cf60a61a3bc25a49bc911fa9e669531750e00935
U5 <kyutech.joko@gmail.com>
2025-03-22 03:26:25 +0900

    callback url変更

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createClient } from "@/utils/supabase/server";
-import { getUser } from "@/utils/supabase/auth";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -24,17 +23,33 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const userData = await getUser();
+      const { data: userData, error: userError } =
+        await supabase.auth.getUser();
+      if (userError) {
+        console.error("Failed to get user data", userError);
+        return NextResponse.redirect(
+          `${process.env.NEXT_PUBLIC_API_URL}/login`
+        );
+      }
       if (userData) {
-        const { id, user_name, display_name, avatar_url } = userData;
-        const { error } = await supabase.from("users").upsert({
-          id: id,
-          user_name: user_name,
-          display_name: display_name,
-          avatar_url: avatar_url,
-        });
-        if (error) {
-          console.error("Failed to upsert user", error);
+        if (userData.user) {
+          const { id, user_metadata } = userData.user;
+          const {
+            user_name,
+            full_name: display_name,
+            avatar_url,
+          } = user_metadata;
+          const { error } = await supabase.from("users").upsert({
+            id: id,
+            user_name: user_name,
+            display_name: display_name,
+            avatar_url: avatar_url,
+          });
+          if (error) {
+            console.error("Failed to upsert user", error);
+          }
+        } else {
+          console.error("User data is null");
         }
       } else {
         console.error("Failed to exchange code for session", error);

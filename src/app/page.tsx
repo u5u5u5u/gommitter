71c2ff40902b5777fb5c71b5c8108bb92b828ae8
U5 <kyutech.joko@gmail.com>
2025-03-22 00:35:36 +0900

    投稿をソート

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default async function Home() {
   const supabase = await createClient();
   const { data, error } = await supabase
     .from("commits")
-    .select("*, user_id(id, display_name, avatar_url)");
+    .select("*, user_id(id, display_name, avatar_url)")
+    .order("created_at", { ascending: false });
+  
   if (error) {
     console.error(error);
     return <div>Failed to fetch data</div>;
